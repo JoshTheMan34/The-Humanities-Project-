8f1b7a0e400090c8c9b0427adc96e37d798700ef
Joshua Fugate <joshua.fugate@students.jmcss.org>
2024-05-04 01:16:26 -0500

    Commit of 5/4/2024
    God has blessed me to get a lot done!

--- index-script.js
+++ index-script.js
@@ -62,3 +62,32 @@
     
 // Type Writer Effect //
 // End //
+
+// Logo Slider //
+// Start //
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+// Logo Slider //
+// End //
